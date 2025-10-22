721502711a570da54239e3c8c5afa91e209381f1
lklostermair <lukas.klostermair@gmail.com>
2025-10-22 16:40:52 -0700

    small changes

--- motion_extraction.py
+++ motion_extraction.py
@@ -2,11 +2,27 @@ import sys
 import cv2
 import numpy
 import pathlib as path
+import os
 
 
 def main():
-    # define file path
-    file_path = sys.argv[2]
+    file_path = sys.argv[1]
     
-    if path(file_path).exists() == False or file_path is None:
+    if path.Path(file_path).exists() == False or file_path is None:
+        print(f"File '{file_path}' is empty or doesn't exist")
+        return
+    
 
+    output_path = numpy.concat(file_path, "_motion")
+
+    cap = cv2.VideoCapture(filename=file_path)
+    if not cap.isOpened():
+        print("Failed to open file")
+        return
+    
+    print("Success")
+
+    return
+
+if __name__ == "__main__":
+os.system('echo "motion_extractor.py home/lukas/Videos/Screencasts/')
